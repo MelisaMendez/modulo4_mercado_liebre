f273e964e85bf622032ec498eb2191f4d496a6a1
MelisaMendez <meli68921992@gmail.com>
2021-12-09 19:44:55 -0300

    Heroku

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set ('port',process.env.PORT || 3000)
+
+app.listen(app.get('port'), ()=> console.log('Servidor corriendo en el puerto 3000')); 
+
 app.use(express.static(path.resolve(__dirname, './public')));
-app.listen(3000,() => console.log('Servidor corriendo en el puerto 3000')); 
+
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname +'/views/home.html'));
 })
+
 app.get('/formulario', (req,res) => {
     res.sendFile(path.resolve(__dirname +'/views/register.html'));
 })
