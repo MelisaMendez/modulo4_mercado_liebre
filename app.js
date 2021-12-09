ab8167749f5843d181a2dbc768cbfc1e1bdcd58e
MelisaMendez <meli68921992@gmail.com>
2021-12-09 18:57:37 -0300

    mensaje

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ app.listen(3000,() => console.log('Servidor corriendo en el puerto 3000'));
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname +'/views/home.html'));
 })
+app.get('/formulario', (req,res) => {
+    res.sendFile(path.resolve(__dirname +'/views/register.html'));
+})
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname + '/views/login.html'));
+})
+
+
